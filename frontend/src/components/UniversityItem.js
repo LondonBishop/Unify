77a79f62585c9b4e0e2bb96e145e310679c6c42a
mariamjaludi <mariam.jaludi@gmail.com>
2019-05-07 16:56:20 +0100

    1.6.1

--- frontend/src/components/UniversityItem.js
+++ frontend/src/components/UniversityItem.js
@@ -3,70 +3,19 @@ import React from 'react'
 export default class UniversityItem extends React.Component {
 
   render(){
+    const {university} = this.props
     return (
-      <div className="ui divided items">
         <div className="item">
           <a className="ui tiny image">
             <img src="/images/avatar/large/stevie.jpg" />
           </a>
           <div className="content">
-            <a className="header">Stevie Feliciano</a>
+            <a className="header">{university.name}</a>
             <div className="description">
-              <p>Stevie Feliciano is a <a>library scientist</a> living in New York City. She likes to spend her time reading, running, and writing.</p>
+              <p>Location: {university.location}</p>
             </div>
           </div>
         </div>
-        <div className="item">
-          <a className="ui tiny image">
-            <img src="/images/avatar/large/stevie.jpg" />
-          </a>
-          <div className="content">
-            <a className="header">Stevie Feliciano</a>
-            <div className="description">
-              <p>Stevie Feliciano is a <a>library scientist</a> living in New York City. She likes to spend her time reading, running, and writing.</p>
-            </div>
-          </div>
-        </div>
-      </div>
     )
   }
 }
-
-
-
-
-// <div class="ui items">
-//   <div class="item">
-//     <a class="ui tiny image">
-//       <img src="/images/avatar/large/stevie.jpg">
-//     </a>
-//     <div class="content">
-//       <a class="header">Stevie Feliciano</a>
-//       <div class="description">
-//         <p>Stevie Feliciano is a <a>library scientist</a> living in New York City. She likes to spend her time reading, running, and writing.</p>
-//       </div>
-//     </div>
-//   </div>
-//   <div class="item">
-//     <a class="ui tiny image">
-//       <img src="/images/avatar/large/veronika.jpg">
-//     </a>
-//     <div class="content">
-//       <a class="header">Veronika Ossi</a>
-//       <div class="description">
-//         <p>Veronika Ossi is a set designer living in New York who <a>enjoys</a> kittens, music, and partying.</p>
-//       </div>
-//     </div>
-//   </div>
-//   <div class="item">
-//     <a class="ui tiny image">
-//       <img src="/images/avatar/large/jenny.jpg">
-//     </a>
-//     <div class="content">
-//       <a class="header">Jenny Hess</a>
-//       <div class="description">
-//         <p>Jenny is a student studying Media Management at <a>the New School</a>.</p>
-//       </div>
-//     </div>
-//   </div>
-// </div>
--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import Profiler from '../components/Profiler'
+import React, {Component} from 'react';
 
+import Profiler from '../components/Profiler'
+import SearchResults from '../components/SearchResults'
+
+const UNI_API = 'http://localhost:3000/universities'
 export default class MainContainer extends Component {
-    state = {
-        predictedGrades: [],
-        selectedUnis:[],
-        searchText: ""
-      }
-      
-    
-    render() {
-        return (
-            <Profiler />
-            // <SearchResults />
-        );
-    }
+
+  state = {
+    universities: [],
+    predictedGrades: [],
+    selectedUnis: [],
+    searchText: ""
+  }
+
+  componentDidMount(){
+    fetch(UNI_API)
+      .then(resp => resp.json())
+      .then(universities => this.setState({universities}))
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <Profiler />
+        <SearchResults universities={this.state.universities}/>
+      </div>
+    );
+  }
 }
